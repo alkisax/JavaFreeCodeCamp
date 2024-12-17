77091d258dd69f7d154886f39c586feb112daebe
Alkis <alkisax@gmail.com>
2024-12-17 20:50:17 +0200

    Update FreeCodeCamp project with new changes

--- p8ToDo/script.js
+++ p8ToDo/script.js
@@ -10,14 +10,163 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input") ;
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
+
+//67
+const removeSpecialChars = (string) => {
+    return string.replace(/[^a-zA-Z0-9 ]/g, '');
+  };
+
+//36
+const addOrUpdateTask  = () => {΄
+    if (!titleInput.value.trim()) {
+        alert("Please provide a title");
+        return;
+    }
+    
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+        id: `${removeSpecialChars(titleInput.value).toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: removeSpecialChars(titleInput.value),
+        date: dateInput.value,
+        description: removeSpecialChars(descriptionInput.value),
+    };
+    console.log(taskObj);
+    
+    if (dataArrIndex === -1){
+        taskData.unshift(taskObj);
+    } else {
+        taskData[dataArrIndex] = taskObj;
+    }
+    //61
+    localStorage.setItem("data", JSON.stringify(taskData));
+    updateTaskContainer();
+    reset();
+};
+
+const updateTaskContainer = () => {
+    tasksContainer.innerHTML = "";
+    taskData.forEach(
+        ({ id, title, date, description }) => {
+            tasksContainer.innerHTML += `
+            <div class="task" id="${id}">
+              <p><strong>Title:</strong> ${title}</p>
+              <p><strong>Date:</strong> ${date}</p>
+              <p><strong>Description:</strong> ${description}</p>
+              <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+              <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+            </div>
+          `
+        }
+    );
+}; 
+
+// 42
+const deleteTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+    buttonEl.parentElement.remove();
+    taskData.splice(dataArrIndex,1);
+    //62
+    localStorage.setItem("data", JSON.stringify(taskData));
+};
+
+const editTask = (buttonEl ) => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+    currentTask = taskData[dataArrIndex];
+    titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date;
+    descriptionInput.value = currentTask.description;
+
+    addOrUpdateTaskBtn.innerText = "Update Task";
+
+    taskForm.classList.toggle("hidden");
+};
+
+
+// 30
+const reset = () => {
+    addOrUpdateTaskBtn.innerText = "Add Task"
+    titleInput.value = "";
+    dateInput.value = "";
+    descriptionInput.value = "";
+    taskForm.classList.toggle("hidden");
+    currentTask = {};
+};
+
+//64
+if (taskData.length) {
+    updateTaskContainer()
+}
 
 openTaskFormBtn.addEventListener("click", () => {
     taskForm.classList.toggle("hidden");
 });
 
+closeTaskFormBtn.addEventListener("click", () => {
+    const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value; 
+    const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description
+    if (formInputsContainValues && formInputValuesUpdated) {
+        confirmCloseDialog.showModal();
+    } else {
+        reset();
+    }
+});
 
+cancelBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+});
 
+discardBtn.addEventListener("click", ()=>{
+    confirmCloseDialog.close();
+    // taskForm.classList.toggle("hidden");
+    reset();
+});
+
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addOrUpdateTask();
+
+    // const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    // const taskObj = {
+    //     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    //     title: titleInput.value,
+    //     date: dateInput.value,
+    //     description: descriptionInput.value
+    // };
+    // console.log(taskObj);
+    
+    // if (dataArrIndex === -1){
+    //     taskData.unshift(taskObj);
+    // }
+    
+    // taskData.forEach(({ id, title, date, description }) => {
+    //     tasksContainer.innerHTML += `
+    //         <div class="task" id="${id}">
+    //             <p><strong>Title:</strong>${title}</p>
+    //             <p><strong>Date:</strong>${date}</p>
+    //             <p><strong>Description:</strong>${description}</p>
+    //             <button type="button" class="btn">Edit</button>
+    //             <button type="button" class="btn">Delete</button>
+    //         </div>
+    //     `;
+    // });
+
+    // // taskForm.classList.toggle("hidden");
+    // reset();
+});
+
+const myTaskArr = [
+    { task: "Walk the Dog", date: "22-04-2022" },
+    { task: "Read some books", date: "02-11-2023" },
+    { task: "Watch football", date: "10-08-2021" },
+];
+localStorage.setItem("data", JSON.stringify(myTaskArr));
+// localStorage.removeItem("data");
+localStorage.clear();
+const getTaskArr = localStorage.getItem("data");
+console.log(getTaskArr);
+const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
+console.log(getTaskArrObj);
 
 
