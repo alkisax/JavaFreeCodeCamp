59e9c00778d69ad899d2e18b755d27bd33e85aef
Alkis <alkisax@gmail.com>
2024-12-30 17:59:33 +0200

    number validator finished

--- TelephoneNumberValidator/script.js
+++ TelephoneNumberValidator/script.js
@@ -3,8 +3,24 @@ const check = document.getElementById("check-btn");
 const clear = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 
-const regEx = /^1?\s?[\-\(]?[023456789][1-9]{2}[\s\-\)]?[1-9]{3}[\s\-]?[1-9]{4}/gi
+const regEx = /^1?\s?(\([023456789][1-9]{2}\)[\s\-]?|[023456789][1-9]{2}[\s\-]?)\s?[1-9]{3}[\s\-]?[1-9]{4}$/i;
 
+/*
+const regEx = new RegExp(
+    [
+        "^1?",                          // Optional leading '1'
+        "\\s?",                         // Optional whitespace
+        "(\\([023456789][1-9]{2}\\)[\\s\\-]?|", // Area code enclosed in parentheses
+        "[023456789][1-9]{2}[\\s\\-]?)",       // Or area code without parentheses
+        "\\s?",                         // Optional additional whitespace
+        "[1-9]{3}",                     // Three digits (1-9)
+        "[\\s\\-]?",                    // Optional whitespace or '-'
+        "[1-9]{4}",                     // Four digits (1-9)
+        "$"                             // End of the string
+    ].join(""),
+    "i" // Case-insensitive flag
+);
+*/
 
 check.addEventListener("click", () => {
     phonechecker(input.value);
