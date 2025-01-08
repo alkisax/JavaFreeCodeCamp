501e075c2d6b7a9b87d0be0751c9827c40ddaf84
Alkis <alkisax@gmail.com>
2025-01-08 11:18:10 +0200

    minor

--- CashRegister/script.js
+++ CashRegister/script.js
@@ -1,3 +1,4 @@
+let statusC = "CLOSED"
 let price = 1.87;
 let cid = [
   ['PENNY', 1.01],
@@ -10,6 +11,17 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
+const values = [
+    0.01,  // PENNY
+    0.05,  // NICKEL
+    0.1,   // DIME
+    0.25,  // QUARTER
+    1,     // ONE
+    5,     // FIVE
+    10,    // TEN
+    20,    // TWENTY
+    100    // ONE HUNDRED
+  ];
 
 const cash = document.getElementById("cash");
 const change = document.getElementById("change-due");
@@ -19,19 +31,20 @@ const sufficientMoneyChecker = () => {
     let customerCash = parseFloat(cash.value);
     if (price > customerCash) {
         alert("Customer does not have enough money to purchase the item")
-        return false;
+        return "INSUFFICIENT FUNDS";;
     } else if (price === customerCash) {
         change.innerText = "No change due - customer paid with exact cash"
-        return true;
+        return "CLOSED";
     } else {
-        return true;
+        return "OPEN";
     }
 }
 
 const changeCalculator = () => {
     const resultArray = [];
     let customerCash = parseFloat(cash.value);
-    if (sufficientMoneyChecker()) {
+    statusC = sufficientMoneyChecker();
+    if (statusC === "OPEN") {
         let moneyChange = customerCash - price;
         resultArray[8] = Math.floor(moneyChange / 100);
         moneyChange %= 100;
@@ -50,9 +63,21 @@ const changeCalculator = () => {
         resultArray[1] = Math.floor(moneyChange / 0.05);
         moneyChange %= 0.05;
         resultArray[0] = Math.floor(moneyChange / 0.01);
+        return resultArray;
     } else {
         return;
     }
+}
+
+const changeCreator = (resultArray) => {
+    let resultString = `Status: ${statusC} `
+    for (let i = 0; i < 9; i++) {
+        if (resultArray[i] <= cid[i][1] / values[i]){
+            const denominationTotal  = resultArray[i] * values[i];
+            resultString += `${cid[i][0]}: \$${denominationTotal.toFixed(2)} `;
+        }
+    }
+    change.innerText = resultString;
 }
 
 button.addEventListener("click", () => {
