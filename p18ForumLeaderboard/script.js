d4a7422e5dc850dcf558d58f1745d93fafbb0ac2
Alkis <alkisax@gmail.com>
2025-01-11 18:16:44 +0200

    minor

---
+++ p18ForumLeaderboard/script.js
@@ -0,0 +1,29 @@
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json",
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+const postsContainer = document.getElementById("posts-container");
+
+/*
+You can use the async keyword to create an asynchronous function, which returns a promise.
+
+const example = async () => {
+  console.log("this is an example");
+};
+
+The try block is designed to handle potential errors, and the code inside the catch block will be executed in case an error occurs.
+
+try {
+  const name = "freeCodeCamp";
+  name = "fCC";
+} catch (err) {
+  console.log(err); // TypeError: Assignment to constant variable.
+}
+*/
+const fetchData = async () => {
+  try{
+
+  } catch(err){
+    
+  }
+};
