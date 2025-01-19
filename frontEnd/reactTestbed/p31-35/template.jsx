b0eeab1fb5da986a749f9b0c2e9e25038cec0320
Alkis <alkisax@gmail.com>
2025-01-19 19:22:33 +0200

    41

--- frontEnd/reactTestbed/p31-35/template.jsx
+++ frontEnd/reactTestbed/p31-35/template.jsx
@@ -137,10 +137,10 @@ class MyComponent extends React.Component {
   }
   // Change code below this line
   componentDidMount() {
-
+      document.addEventListener("keydown", this.handleKeyPress)
   }
   componentWillUnmount() {
-
+    document.removeEventListener("keydown", this.handleKeyPress)
   }
   // Change code above this line
   handleEnter() {
@@ -161,6 +161,50 @@ class MyComponent extends React.Component {
     );
   }
 };
+//35
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+    // Change code below this line
+    if (nextProps.value % 2 === 0){
+      return true;
+    }
+    return false;
+    // Change code above this line
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
 //
 // const container = document.getElementById("container")
 // ReactDOM.render(<MyComponent />, container)
